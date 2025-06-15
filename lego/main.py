e5a6269268e3dc81ace73f7a2d33af10c068d351
FLoFLoOO7 <florian.beldi@gmail.com>
2025-06-15 13:30:21 +0200

    einfach mal commiten

--- lego/main.py
+++ lego/main.py
@@ -8,21 +8,13 @@ from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
 
-motoro_left=Motor(Port.C)
-motoro=Motor(Port.D)
-#motoro.run_angle(100,360)
-#motoro_left.run_angle(1000,-160)
-c=0
-d=0
-done=True
-while c<=100:
-    motoro_left.run_angle(1000,140)
-    motoro_left.run_angle(1000,-140)
-    a=randint(0,20)
-    if a<10:
-        if a<10:
-            k=180
-        else:
-            k=90
-        motoro.run_angle(1000,k)
-        c+=1
+# This program requires LEGO EV3 MicroPython v2.0 or higher.
+# Click "Open user guide" on the EV3 extension tab for more information.
+
+
+# Create your objects here.
+ev3 = EV3Brick()
+
+
+# Write your program here.
+ev3.speaker.beep()
